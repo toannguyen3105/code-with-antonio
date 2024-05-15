f35b0f751695beb0a96354583434f2a9dc8171c6
tony <nguyenhuytoan1994@outlook.com>
2024-05-15 22:14:04 +0700

    Add drizzle ORM

---
+++ finance-tutorial/db/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, text } from "drizzle-orm/pg-core";
+
+const accounts = pgTable("accounts", {
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+});
+
+export { accounts };
