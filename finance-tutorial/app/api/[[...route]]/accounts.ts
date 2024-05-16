66cfd6b4ac749aa00e97070c81dcb0ad351c6471
tony <nguyenhuytoan1994@outlook.com>
2024-05-17 00:18:34 +0700

    Update accounts err msg

--- finance-tutorial/app/api/[[...route]]/accounts.ts
+++ finance-tutorial/app/api/[[...route]]/accounts.ts
@@ -10,9 +10,7 @@ const app = new Hono().get("/", clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
 
   if (!auth?.userId) {
-    throw new HTTPException(401, {
-      res: c.json({ error: "Unauthorized" }, 401),
-    });
+    return c.json({ error: "Unauthorized" }, 401);
   }
 
   const data = await db
---
+++ finance-tutorial/run.sh
@@ -0,0 +1,1 @@
+rsync -avzh /home/tony/Frontends/code-with-antonio/finance-tutorial/.next/* toannh@45.76.174.96:/home/toannh/deployments/code-with-antonio/finance-tutorial/.next/
